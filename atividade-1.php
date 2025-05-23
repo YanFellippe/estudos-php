91009e7bd5abe97286a9b7ee38345d780a00506f
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2025-05-23 09:28:46 -0300

    Add files via upload

---
+++ atividade-1.php
@@ -0,0 +1,13 @@
+<?php
+echo "Olá mundo!<br>";
+echo "Nasceu na rede, com um sonho a cumprir, entre scripts e tags a se expandir.<br>
+PHP, linguagem de gente valente, que roda no back-end, firme e presente.<br>
+Em meio ao HTML, se faz poesia, com lógica pura e sintonia.<br>
+Do `echo` ao `foreach`, com simplicidade, constrói sistemas, sonhos, realidade.<br>
+Com variáveis flexíveis na mão, cria sessões, conexões, solução.<br>
+Na dança do `if`, no laço do `while`, resolve o problema com jeito e com trial.<br>
+Integra com banco, responde ao usuário, é leve, é veloz, quase um visionário.<br>
+De blogs antigos a lojas robustas, é o motor oculto de ideias justas.<br>
+Criticado por uns, amado por muitos, ainda respira em códigos distintos.<br>
+Pois enquanto houver servidor no ar, PHP, velho amigo, vai continuar.<br>";
+?>
