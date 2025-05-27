2eefe567a1b1f92b46988a368497cc2512d61164
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2025-05-27 10:04:47 -0300

    Add files via upload

--- atividade-12.php
+++ atividade-12.php
@@ -1,7 +1,7 @@
 <?php
 $numero = 10;
-$dobro = $numero * $numero;
-$triplo = $numero * $numero * $numero;
+$dobro = $numero + $numero;
+$triplo = $numero + $numero + $numero;
 
 echo "O número escolhido foi: ". $numero."<br>";
 echo "O dobro do número escolhido foi: ". $dobro."<br>";
