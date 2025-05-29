296b00a24443f3a87980b25e1bedd4227643a237
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2025-05-29 09:23:03 -0300

    Add files via upload

---
+++ atividade-14.php
@@ -0,0 +1,11 @@
+<?php
+$num = 7;
+
+if($num > 0){
+    echo "Você escolheu um número positivo";
+} else if($num = 0){
+    echo "Você escolheu o número ZERO";
+} else {
+    echo "Você escolheu um número negativo";
+}
+?>
